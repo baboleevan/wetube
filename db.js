8c406e58b4eccc9377253d0e785de85040c97394
rlarnfktkd1 <ksj8367@gmail.,com>
2019-04-19 21:40:50 +0900

    mongoose-setting

--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,
     {
-        id: 324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangheon Kim",
-            email: "ksj8367@gmail.com"
-        }
-    },
-    {
-        id: 121231,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangheon Kim",
-            email: "ksj8367@gmail.com"
-        }
-    },
-    {
-        id: 111111,
-        title: 'Video sad',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangheon Kim",
-            email: "ksj8367@gmail.com"
-        }
-    },
-    {
-        id: 3243214,
-        title: 'Video ugly',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Sangheon Kim",
-            email: "ksj8367@gmail.com"
-        }
-    },
-]
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔️  Connected to DB");
+const handleError = (error) => console.log(`❌  Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
