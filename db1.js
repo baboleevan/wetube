15d1adc88c7c886d905e846df67f90a6cd4ba95d
rlarnfktkd1 <ksj8367@gmail.,com>
2019-04-19 21:02:45 +0900

    add 설명 추가

---
+++ db1.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Sangheon Kim",
+            email: "ksj8367@gmail.com"
+        }
+    },
+    {
+        id: 121231,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Sangheon Kim",
+            email: "ksj8367@gmail.com"
+        }
+    },
+    {
+        id: 111111,
+        title: 'Video sad',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Sangheon Kim",
+            email: "ksj8367@gmail.com"
+        }
+    },
+    {
+        id: 3243214,
+        title: 'Video ugly',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Sangheon Kim",
+            email: "ksj8367@gmail.com"
+        }
+    },
+]
